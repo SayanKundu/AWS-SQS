4c94d16da7517b6b8393b5eac812689cc8d7b276
SAYAN <you@example.com>
2020-10-08 12:44:51 +0530

    Final Commit

--- aws-sqs/src/main/java/com/sayan/aws/controller/SQSController.java
+++ aws-sqs/src/main/java/com/sayan/aws/controller/SQSController.java
@@ -8,11 +8,15 @@ import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("v1/api/sqs")
 public class SQSController {
+	
 
 	Logger logger = LoggerFactory.getLogger(SQSController.class);
 
@@ -24,16 +28,22 @@ public class SQSController {
 	@Value("${cloud.aws.end-point.uri}")
 	private String endpoint;
 
-	@GetMapping("/send/{message}")
-	public void sendMessageToQueue(@PathVariable String message) {
+	@GetMapping
+	public void sendMessageToQueue() {
 		// This message I need to send using queueMessagingTemplate
-		queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
+		queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload("Hello from AWS SQS Service").build());
+	}
+	
+	@PostMapping
+	public void postMessage(@RequestBody String msg) {
+		queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(msg).build());
 	}
 
 	// CONSUMES THE MESSAGE
+	//GIVE THE QUEUE NAME
 	@SqsListener("Java_Sayan_1")
 	public void loadMessageFromSQS(String message) {
-		logger.info("message from SQS" + message);
+		logger.info("Received from AWS SQS Queue - " + message);
 	}
 
 }
